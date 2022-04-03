3ccf4963e8ceb83f88698c6e3c6807ae5a96cc35
Aleksej V <proglamer@gmail.com>
2022-04-03 23:01:09 +0300

    Dummy TimeEntry controller.

--- TimeKeeperServerApi/src/TimeKeeperServerApi/Controllers/TimeEntriesController.cs
+++ TimeKeeperServerApi/src/TimeKeeperServerApi/Controllers/TimeEntriesController.cs
@@ -17,16 +17,11 @@ namespace TimeKeeperServerApi.Controllers
     [Route("api/[controller]")]
     public class TimeEntriesController : ControllerBase
     {
-        private readonly IAmazonDynamoDB client;
-        private readonly DynamoDBContext context;
-
-        public TimeEntriesController(/*IAmazonDynamoDB client*/)
+        public TimeEntriesController()
         {
-            // this.client = client;
-            // this.context = new DynamoDBContext(client);
         }
 
-        // GET api/books
+        // GET api/timeentries
         [HttpGet]
         public async Task<IEnumerable<TimeEntryDto>> Get()
         {
@@ -39,80 +34,32 @@ namespace TimeKeeperServerApi.Controllers
                    
                 }
             };
-
-            // ScanFilter filter = new ScanFilter();
-            // filter.AddCondition("Title", ScanOperator.IsNotNull);
-            // ScanOperationConfig scanConfig = new ScanOperationConfig
-            // {
-            //     Limit = 10,
-            //     Filter = filter
-            // };
-            // var queryResult = context.FromScanAsync<Book>(scanConfig);
-
-            // do
-            // {
-            //     result.AddRange(await queryResult.GetNextSetAsync());
-            // }
-            // while (!queryResult.IsDone && result.Count < 10);
-
             return result;
         }
 
-        // GET api/books/5
+        // GET api/timeentries/5
         [HttpGet("{id}")]
-        public async Task<Book> Get(Guid id)
+        public async Task<TimeEntryDto> Get(string id)
         {
-            LambdaLogger.Log($"Looking for book {id}");
-            return await context.LoadAsync<Book>(id);
+            LambdaLogger.Log($"Looking for TimeEntry by ID {id}");
+            return new TimeEntryDto{
+                TimeEntryId = id
+            };
         }
 
-        // POST api/books
+        // POST api/timeentries
         [HttpPost]
-        public async Task Post([FromBody] Book book)
+        public async Task<TimeEntryDto> Post([FromBody] TimeEntryDto entry)
         {
-            if (book == null )
+            if (entry == null )
             {
-                throw new ArgumentException("Invalid input! Book not informed");
+                throw new ArgumentException($"Invalid input! {typeof(TimeEntryDto).Name} not informed");
             }
 
-            await context.SaveAsync<Book>(book);
-            LambdaLogger.Log($"Book {book.Id} is added");
-        }
+            entry.TimeEntryId = Guid.NewGuid().ToString("N");
 
-        // PUT api/books/5
-        [HttpPut("{id}")]
-        public async Task Put(Guid id, [FromBody] Book book)
-        {
-            // Retrieve the book.
-            Book bookRetrieved = await context.LoadAsync<Book>(id);
-
-            if (bookRetrieved == null)
-            {
-                var errorMsg = $"Invalid input! No book found with id:{id}";
-                LambdaLogger.Log(errorMsg);
-                throw new ArgumentException(errorMsg);
-            }
-
-            book.Id = bookRetrieved.Id;
-
-            await context.SaveAsync<Book>(book);
-            LambdaLogger.Log($"Book {book.Id} is updated");
-        }
-
-        // DELETE api/books/5
-        [HttpDelete("{id}")]
-        public async Task Delete(Guid id)
-        {
-            // Delete the book.
-            await context.DeleteAsync<Book>(id);
-            // Try to retrieve deleted book. It should return null.
-            Book deletedBook = await context.LoadAsync<Book>(id, new DynamoDBContextConfig
-            {
-                ConsistentRead = true
-            });
-
-            if (deletedBook == null)
-                LambdaLogger.Log($"Book {id} is deleted");
+            // LambdaLogger.Log($"TimeEntry {entry.TimeEntryId} is added");
+            return entry;
         }
     }
 }
